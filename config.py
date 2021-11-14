e8d48446e9361ee6eed5834d742b1986d85d39f8
blkj <zhaopeng199812.@163.com>
2021-11-14 17:10:07 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+root_of_aircraft = "F:/datasets/aircraft/"  # The path to store the aircraft dataset
+root_of_bird = "F:/datasets/birds/"  # The path to store the bird dataset
+root_of_car = "F:/datasets/cars/Stanford_Cars1/"  # The path to store the car dataset
+
+# Path to store pre-trained models
+pretrain_pth = "F:/zp_experiments/github/TFFF-master/parameters/resnet_pretrained_model/resnet50_pretrained.pth"
